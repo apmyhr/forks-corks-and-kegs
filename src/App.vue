de8132a0118d7ffd10ba7a93f3815ffe33659be2
apmyhr <aaron.myhr@live.com>
2019-12-08 19:04:20 -0500

    Replace logo

--- src/App.vue
+++ src/App.vue
@@ -48,9 +48,9 @@
       <section>
         <v-parallax src="@/assets/wine_cheese2.jpg" height="600">
           <v-layout column align-center justify-center class="white--text">
-            <img src="@/assets/BeerandWine-566x392.png" alt="Vuetify.js" height="200" />
             <h1 class="white--text mb-2 display-1 text-center">{{title}}</h1>
             <div class="subheading mb-4 text-center">{{date}}</div>
+            <img src="@/assets/logo.png" alt="Vuetify.js" height="300" />
             <v-btn class="mt-12" color="blue lighten-2" dark large @click="registerDialog = true">Register</v-btn>
           </v-layout>
         </v-parallax>
