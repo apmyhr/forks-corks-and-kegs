b6e4f87c2adfe12f33f07a5da6c006eaa05aedc7
apmyhr <aaron.myhr@live.com>
2022-01-25 20:53:05 -0500

    update pdf fix 2023 year type

--- src/App.vue
+++ src/App.vue
@@ -404,7 +404,7 @@
 
     <v-dialog v-model="viewFlyerDialog">
       <embed
-        src="20-House-ForksCorksKegs-Flyer.pdf"
+        :src="pdfName"
         style="width: calc(100%); height: calc(100vh - 150px)"
       />
     </v-dialog>
@@ -458,7 +458,9 @@
 
 import auctionJson from "./auctionThanks.json";
 
-const HANDBID_WEBSITE = "https://events.handbid.com/auctions/forks-corks-and-kegs-2022";
+const HANDBID_WEBSITE =
+  "https://events.handbid.com/auctions/forks-corks-and-kegs-2022";
+const PDF_NAME = "ForksCorksKegs.pdf";
 
 export default {
   name: "App",
@@ -504,7 +506,7 @@ export default {
         title: "Tickets",
         text: "Tickets",
         paragraphs: [
-          `Tickets can be purchased on the <a href='${HANDBID_WEBSITE}'>Handbid website</a> for $60 before May 20th, and $70 the week of the event. Each ticket includes all you can eat food at the food trucks, all you can drink beer and wine, and a tasting glass.`,
+          `Tickets can be purchased on the <a href='${HANDBID_WEBSITE}'>Handbid website</a> for $55 during Catholic Schools Week Special, Jan. 31 - Feb 6th, $65 before May 15th, and $75 the week of the event. Each ticket includes all you can eat food at the food trucks, all you can drink beer and wine, and a tasting glass.`,
         ],
       },
       {
@@ -512,7 +514,7 @@ export default {
         text: "Sponsors",
         paragraphs: [
           "​​Thank you to this year’s generous sponsors! Business Sponsors were",
-          "If you are interested in being a sponsor for 2023, please <a href='mailto:lofar9ex56@gmail.com'>contact us</a> for details",
+          "If you are interested in being a sponsor for 2022, please <a href='mailto:lofar9ex56@gmail.com'>contact us</a> for details",
         ],
         photos: [
           "/sponsors/MSB.png",
@@ -628,6 +630,7 @@ export default {
     // registrationEmail: "",
     registrationInterestedIn: [],
     handbidWebsite: HANDBID_WEBSITE,
+    pdfName: PDF_NAME,
   }),
   computed: {
     auctionThanksJson() {
@@ -650,7 +653,7 @@ export default {
       });
     },
     openFlyer() {
-      window.open("20-House-ForksCorksKegs-Flyer.pdf", "_blank");
+      window.open(PDF_NAME, "_blank");
     },
     openUrl(URL) {
       window.open(URL, "_blank");
