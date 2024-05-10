0a02952171f272dc7dbb526840f8a713348efbf7
apmyhr <aaron.myhr@live.com>
2024-05-09 21:22:10 -0400

    update sponsors support links for sponsors

--- src/components/SponsorsList.vue
+++ src/components/SponsorsList.vue
@@ -6,9 +6,10 @@
     class="mt-0 pt-0"
   >
     <v-list-item v-for="(sponsor, index) in sponsors" :key="index">
-      <v-list-item-title class="text-lg-h4 headline">{{
-        sponsor
-      }}</v-list-item-title>
+      <v-list-item-title
+        class="text-lg-h4 headline"
+        v-html="getLink(sponsor)"
+      />
     </v-list-item>
   </v-list>
 </template>
@@ -18,6 +19,15 @@ export default {
   name: "SponsorsList",
   props: {
     sponsors: Array,
+  },
+  methods: {
+    getLink(sponsor) {
+      if (typeof sponsor.link === "string" && sponsor.link) {
+        return `<a href="${sponsor.link}" target="_blank">${sponsor.title}</a>`;
+      } else {
+        return sponsor.title ?? sponsor;
+      }
+    },
   },
 };
 </script>
